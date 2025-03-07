c37b856403acf022bf652185ca85731a4a7a38be
cc-jitters <ayankhan1186@gmail.com>
2025-03-07 09:52:37 +0530

    CI/CD Pipeline through GitHub Actions.

---
+++ src/test/java/com/javatechie/cicd_github_actions/CicdGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.cicd_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
